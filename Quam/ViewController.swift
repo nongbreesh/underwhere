0a2c8e3fbdddb6214904093ab062c9ba5695b9aa
Breeshy <breesh.comsci@gmail.com>
2014-11-09 18:02:34 +0700

    Initial Commit

---
+++ Quam/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Quam
+//
+//  Created by Breeshy Sama on 11/9/2557 BE.
+//  Copyright (c) 2557 Breeshy Sama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
